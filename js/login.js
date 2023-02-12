185a6537e3862f2e73d7a0e2af91b12dea3c2708
faisalakonda <faisalakonda2@gmail.com>
2023-02-12 18:33:00 +0600

    Login Dashboard

---
+++ js/login.js
@@ -0,0 +1,19 @@
+// step-1: add click event handler with the submit button 
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // Step-2: get the email address inside the email input field 
+    // always remember to use .value to get text from an input field 
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3: get password 
+    // step-3.a. set id on the html element 
+    // step-3.b. get the value from the element 
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+    if(email === 'example@mail.com' && password === '1234') {
+        window.location.href = 'bank.html';
+    }
+    else {
+        alert('invalid user')
+    }
+})
